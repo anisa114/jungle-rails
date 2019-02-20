9870c47238a4f30f41b24dffbd37887fc4478e6d
Anisa Tahlil <anisa.tahlil22@gmial.com>
2019-02-20 18:00:54 -0500

    Feature: Admin Categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,29 @@
+class Admin::CategoriesController < ApplicationController
+
+    def index
+        @categories = Category.all.order(created_at: :desc)
+    end
+    
+    def new
+        @category = Category.new
+    end
+    
+    def create
+        @category = Category.new(category_params)
+        
+        if @category.save
+            redirect_to @category
+          else
+            # This line overrides the default rendering behavior, which
+            # would have been to render the "create" view.
+            render "new"
+          end
+
+    end
+
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
